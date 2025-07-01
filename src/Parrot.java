9930381b5987f33bfa38393ceac916dc2d134d98
Juan A. García <juan.a.garcia.gomez@gmail.com>
2025-07-01 18:58:47 +0200

    added Parrot class

---
+++ src/Parrot.java
@@ -0,0 +1,9 @@
+public class Parrot extends Pet{
+
+    @Override
+    public String communicate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'communicate'");
+    }
+    
+}
